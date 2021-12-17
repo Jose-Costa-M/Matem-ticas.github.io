3f9752534118b0061012467f670ce5cbc8598a00
José Costa <88689761+Jose-Costa-M@users.noreply.github.com>
2021-12-16 20:18:01 -0600

    Add files via upload

---
+++ js/resp9.js
@@ -0,0 +1,71 @@
+
+
+function verificarRespuestas() {
+
+
+    var total = 10;
+    var puntos = 0;
+    var myForm = document.forms["quizForm"];
+    var respuestas = ["c", "a", "c", "a", "c", "b", "c", "c", "a", "d"];
+    var error = []; 
+
+    for (var i = 1; i <= total; i++) {
+        if (myForm["p" + i].value === null || myForm["p" + i].value === "") {
+            Swal.fire({
+                title : 'Por favor responde la pregunta '+i,
+                icon : 'warning',
+                footer: '<span class ="title">Verifica si todas las preguntas tienen respuesta :)</span>',
+                allowOutsideClick: true,
+                allowEscapeKey: true,
+                allowEnterKey: true,
+                stopKeydownPropagation: false,
+    
+                
+            });
+
+            return false;
+        }
+        else {
+            if (myForm["p" + i].value === respuestas[i - 1]) {
+                puntos++;
+            }
+            else
+            {
+                error.push(i); 
+            }
+        }
+    }
+   
+  if(error.length === 0)
+  {
+      error.push(0); 
+  }
+  
+    
+    document.getElementById('submit').disabled=true;
+
+
+    var resultado = document.getElementById("resultado");
+    resultado.innerHTML = '<h3>Obtuviste <span>' + puntos + '</span> de <span>' + total + 'puntos</span></br>Preguntas erroneas: <span>'+error+', '+'</span> </h3>';
+    
+   
+
+    return false;
+
+}
+
+
+$("#submit").click(function(){
+
+    Swal.fire(
+        {
+            title : 'Examen completado, verifica tus respuestas.',
+            icon : 'success',
+            allowOutsideClick: true,
+            allowEscapeKey: true,
+            allowEnterKey: true,
+            stopKeydownPropagation: false,
+        }
+    ); 
+
+} ); 
